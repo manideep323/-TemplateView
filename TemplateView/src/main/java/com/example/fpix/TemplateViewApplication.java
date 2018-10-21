3efa7cf9113f5a2ab96c97423c9976ff068c4d16
manideep323 <manideep323@manideep>
2018-10-21 20:28:54 +0530

    project created first commit

---
+++ TemplateView/src/main/java/com/example/fpix/TemplateViewApplication.java
@@ -0,0 +1,12 @@
+package com.example.fpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateViewApplication.class, args);
+	}
+}
