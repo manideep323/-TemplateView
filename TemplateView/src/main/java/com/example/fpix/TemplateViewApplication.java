4cf2284630c6604532d635ba990c7b7c06d44c7b
manideepg <manideepg@fortunapix.com>
2018-10-22 12:06:42 +0530

    added comment

--- TemplateView/src/main/java/com/example/fpix/TemplateViewApplication.java
+++ TemplateView/src/main/java/com/example/fpix/TemplateViewApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemplateViewApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateViewApplication.class, args);
 	}
