7070a401419e45ec46d96151ad3b4d4007bb0b16
manideep323 <manideep323@manideep>
2018-10-23 19:02:50 +0530

    home system commit

--- TemplateView/src/main/java/com/example/fpix/TemplateViewApplication.java
+++ TemplateView/src/main/java/com/example/fpix/TemplateViewApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemplateViewApplication {
-//test
+//from home system
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateViewApplication.class, args);
 		
